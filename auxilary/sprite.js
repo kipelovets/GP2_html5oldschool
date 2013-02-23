62dca64e4bbe2d700b43cd6c9373d89ff2b68096
kipelovets <kipelovets@mail.ru>
2013-02-23 19:55:01 +0400

    WIP

--- auxilary/sprite.js
+++ auxilary/sprite.js
@@ -4,54 +4,70 @@
 //-----------------------------------------
 
 
-Sprite = function( config )
-{
-    this.initialize(config);
-}
+!function(){
 
-Sprite.prototype.initialize = function(config)
-{
-
-    this.frames = [];
-    this.valid  = false;
-    this.currentFrame = 0;
-    this.timer = 0;
-    this.fps = config.fps;
-
-
-    var waiting = config.frames.length;
-    var that  = this;
-
-
-    for (frame in config.frames)
+    Sprite = function( config )
     {
-        var img = new Image();
-        this.frames.push(img);
-
-        img.onload = function()
-        {
-            waiting--;
-
-            if (waiting <=0)
-                that.valid = true;
-        }
-        img.src = config.baseUrl + "/" +config.frames[frame];
+        this.initialize(config);
     }
-}
+    
+    Sprite.prototype.initialize = function(config)
+    {
+    
+        this.frame = 0
+        this.frames = [];
+        this.valid  = false;
+        this.currentFrame = 0;
+        this.timer = 0;
+        this.fps = config.fps;
+    
+    
+        var waiting = config.frames.length;
+        var that  = this;
+    
+    
+        for (frame in config.frames)
+        {
+            var img = new Image();
+            this.frames.push(img);
+    
+            img.onload = function()
+            {
+                waiting--;
+    
+                if (waiting <=0) {
+                    that.valid = true;
+                    if (config.onload) {
+                        config.onload.call(that)
+                    }
+                    that.currentFrame = that.frames[0]
+                }
+            }
+            img.src = config.baseUrl + "/" +config.frames[frame];
+        }
+    }
+    
+    
+    Sprite.prototype.update = function(dt)
+    {
+        if (!this.valid) return;
+    
+        this.timer += dt;
+    
+        var frame = Math.floor( this.timer / (1000/this.fps) ) % this.frames.length;
+        this.currentFrame=this.frames[frame];
+    }
 
+    Sprite.prototype.reset = function () {
+        this.times = 0
+        this.currentFrame = this.frames[0]
+    }
+    
+    Sprite.prototype.draw = function (x, y, w, h) {
+        if (!this.valid) return;
+        if(arguments.length==2)   ctx.drawImage(this.currentFrame, x, y);
+        else   ctx.drawImage(this.currentFrame, x, y, w, h);
+    }
 
-Sprite.prototype.update = function(dt)
-{
-    if (!this.valid) return;
-
-    this.timer += dt;
-
-    var frame = Math.floor( this.timer / (1000/this.fps) ) % this.frames.length;
-    this.currentFrame=this.frames[frame];
-}
-
-Sprite.prototype.draw = function (x, y, w, h) {
-    if (!this.valid) return;
-    if(arguments.length==2)   ctx.drawImage(this.currentFrame, x, y);
-    else   ctx.drawImage(this.currentFrame, x, y, w, h);
-}
+    window.Sprite = Sprite;
+}()
--- game.js
+++ game.js
@@ -2,6 +2,170 @@
 // Base class for the game object
 //-------------------------------------------------------
 
+// Parameters
+var screenHeight      = 640,
+    screenWidth       = 960,
+    carsPerSecond     = 0.3,
+    carSpaceThreshold = 1.0,
+    carsThreshold     = 0.5,
+    roadWidth         = 200,
+    offroadWidth      = 80,
+    roads             = 4,
+    // Car
+    carHeight         = 189,
+
+    stage = 0
+    ;
+
+var Bear = function () {
+    this.spriteLeft = new Sprite({
+        "baseUrl"  : "res/gfx/"
+        , "fps"    : 60
+        , "frames" : [
+            'walk_left/1.png', // 0
+            'walk_left/2.png',
+            'walk_left/3.png',
+            'walk_left/4.png',
+            'walk_left/5.png',
+            'walk_left/6.png',
+            'walk_left/7.png',
+            'walk_left/8.png',
+            'walk_left/9.png',
+            'walk_left/10.png',
+            'walk_left/11.png',
+            'walk_left/12.png',
+            'walk_left/13.png',
+            'walk_left/14.png',
+            'walk_left/15.png',
+            'walk_left/16.png',
+            'walk_left/17.png',
+            'walk_left/18.png',
+            'walk_left/19.png'
+        ]})
+    this.spriteRight = new Sprite({
+        "baseUrl"  : "res/gfx/"
+        , "fps"    : 60
+        , "frames" : [
+            'walk_right/1.png', // 19
+            'walk_right/2.png',
+            'walk_right/3.png',
+            'walk_right/4.png',
+            'walk_right/5.png',
+            'walk_right/6.png',
+            'walk_right/7.png',
+            'walk_right/8.png',
+            'walk_right/9.png',
+            'walk_right/10.png',
+            'walk_right/11.png',
+            'walk_right/12.png',
+            'walk_right/13.png',
+            'walk_right/14.png',
+            'walk_right/15.png',
+            'walk_right/16.png',
+            'walk_right/17.png',
+            'walk_right/18.png',
+            'walk_right/19.png',
+        ]})
+
+    var x = 0, 
+        y = 300, 
+        face = -1,  // -1 - left, 1 - right
+        dx = 0, 
+        sprite = this.spriteLeft, 
+        walking = false
+
+    this.moveLeft = function () {
+        if (!walking && x > 0) {
+            sprite = this.spriteLeft
+            sprite.reset()
+            walking = true
+            face = -1
+        }
+    }
+
+    this.moveRight = function () {
+        if (!walking && x < roads - 1) {
+            sprite = this.spriteRight
+            sprite.reset()
+            walking = true
+            face = 1
+        }
+    }
+
+    this.draw = function () {
+        sprite.draw(offroadWidth + x * roadWidth + face * dx, y)
+    }
+
+    this.update = function (tickperframe) {
+        if (walking) {
+            sprite.update(tickperframe)
+            var walkTime = 0.3, 
+                walkSpeed = roadWidth / walkTime
+            dx += walkSpeed * tickperframe / 1000
+            if (dx >= roadWidth) {
+                walking = 0
+                dx = 0
+                x += face
+            }
+        }
+    }
+
+    this.collide = function (car) {
+        return car.getX() == x && (car.y + carHeight >= y)
+    }
+}
+
+var Car = function () {
+    this.sprites = [
+        new Sprite({
+            "baseUrl"  : "res/gfx/car/"
+            , "fps"    : 24
+            , "frames" : ['car1.png']
+        }),
+        new Sprite({
+            "baseUrl"  : "res/gfx/car/"
+            , "fps"    : 24
+            , "frames" : ['car2.png']
+        })
+    ]
+
+    var sprite = this.sprites[Math.floor(Math.random() * this.sprites.length)]
+
+    this.x = 80 + 200 * Math.floor(Math.random() * 3)
+    this.y = - carHeight
+    this.speed = 100
+
+    this.isOut = false
+
+    this.hasFreeSpace = function () {
+        return this.y > sprite.height * carSpaceThreshold
+    }
+
+    this.getX = function () {
+        return (this.x - offroadWidth) / roadWidth
+    }
+
+    this.draw = function () {
+        sprite.draw(this.x, this.y)
+    }
+
+    this.update = function (tickperframe) {
+        sprite.update(tickperframe) 
+        this.y += this.speed * tickperframe / 1000
+        if (this.y > screenHeight) {
+            this.isOut = true;
+        }
+    }
+}
+
+var sounds = {};
+var bear = new Bear();
+var cars = [];
+var bg;
+var bgPos = 0,
+    lifeImage,
+    startTime = new Date
+    ;
 
 Game = function () {
 
@@ -9,44 +173,114 @@ Game = function () {
 
 Game.prototype.Load = function () {
 
-    // load sound
-    this.SoundJump = new buzz.sound("res/jump.ogg");
-    this.SoundJump.play();
+    bg = new Image()
+    bg.src = 'res/gfx/road.png'
 
-    // load ambient music and play it
-    this.SoundAmbient = new buzz.sound("res/sound.ogg");
-    this.SoundAmbient.loop().play();
+    lifeImage = new Image()
+    lifeImage.src = 'res/gfx/heart.png'
 
-    // set up creature
-    this.creatureImg = new Image();
-    this.creatureImg.src = 'res/creature.png';
+    // // load sound
+    // this.SoundJump = new buzz.sound("res/jump.ogg");
+    // this.SoundJump.play();
 
-    this.creaturePos = new Vec2(canvas.width / 2, canvas.height / 2);
+    // // load ambient music and play it
+    // this.SoundAmbient = new buzz.sound("res/sound.ogg");
+    // this.SoundAmbient.loop().play();
 
-    // set up girl
-    this.girlSpr = new Sprite({
-        "baseUrl"  : "res/girl/"
-        , "fps"    : 30
-        , "frames" : ["girl_01.png", "girl_02.png", "girl_03.png","girl_04.png"
-        , "girl_05.png", "girl_06.png", "girl_07.png", "girl_08.png" 
-        , "girl_09.png", "girl_10.png", "girl_11.png", "girl_12.png"
-        , "girl_13.png", "girl_14.png", "girl_15.png", "girl_16.png" ]
-    });
-    this.girlPos = new Vec2(0, canvas.height / 2);
+    // // set up creature
+    // this.creatureImg = new Image();
+    // this.creatureImg.src = 'res/creature.png';
+
+    // this.creaturePos = new Vec2(canvas.width / 2, canvas.height / 2);
+
+    // // set up girl
+    // this.girlPos = new Vec2(0, canvas.height / 2);
 }
+
+var carsElapsed = 0,
+    lifes = 3,
+    pause = false
+    ;
 
 Game.prototype.Calculate = function () {
-    this.girlPos.x += tickperframe / 5;
-    if (this.girlPos.x > canvas.width) this.girlPos.x = -100;
-    this.girlSpr.update(tickperframe);
+    if (lifes <= 0 || pause) {
+        return
+    }
+
+    bgPos = (bgPos + tickperframe / 5) % screenHeight
+    bear.update(tickperframe)
+
+    // Cars
+    carsElapsed += secperframe
+    var dice = Math.random() * carsElapsed * carsPerSecond
+    if (dice > carsThreshold) {
+        carsElapsed = 0
+        var spaces = [], i, count = 0
+        for (i = offroadWidth; i < screenWidth; i += roadWidth) {
+            spaces[count++] = true
+        }
+        for (i in cars) {
+            spaces[cars[i].getX()] &= cars[i].hasFreeSpace()
+        }
+        var j = Math.floor(Math.random() * spaces.length)
+        for (i = 0; i < count; i++) {
+            if (spaces[(j + i) % count]) {
+                var car = new Car()
+                car.x = offroadWidth + ((j + i) % count) * roadWidth
+                cars.push(car)
+                break
+            }
+        }
+    }
+
+    i = 0
+    while (i < cars.length) {
+        cars[i].update(tickperframe)
+        if (cars[i].isOut) {
+            cars.splice(i, 1)
+        } else {
+            i++
+        }
+    }
+
+    // Collision
+    i = 0
+    while (i < cars.length) {
+        if (bear.collide(cars[i])) {
+            lifes--
+            cars.splice(i, 1)
+            break
+        }
+        i++
+    }
 }
 
+var x = 0;
 
 Game.prototype.Render = function () {
-    
-    ctx.drawImage(this.creatureImg, 0, 0, 128, 128, this.creaturePos.x, this.creaturePos.y, 128, 128);
+    ctx.font = "20px Georgia"
+    if (lifes <= 0) {
+        ctx.fillText("Game Over",450,300)
+        return
+    }
 
-    this.girlSpr.draw(this.girlPos.x, this.girlPos.y);
+    ctx.drawImage(bg, 0, 0, bg.width, bg.height, 0, bgPos, bg.width, bg.height)
+    if (bgPos > 0) {
+        ctx.drawImage(bg, 0, 0, bg.width, bg.height, 0, bgPos - bg.height, bg.width, bg.height)
+    }
+
+    bear.draw()
+
+    var i
+    for (i in cars) {
+        cars[i].draw()
+    }
+
+    for (i = 0; i < lifes; i++) {
+        ctx.drawImage(lifeImage, 0, 0, lifeImage.width, lifeImage.height, 932, 264 + i*44, lifeImage.width, lifeImage.height)
+    }
+
+    ctx.fillText(Math.floor((new Date - startTime) / 1000), 5, 620)
 }
 
 //---------------------------------------------
@@ -55,12 +289,12 @@ Game.prototype.Render = function () {
 Game.prototype.onmousedown = function (e) {
 
     // get pos from event e
-    this.creaturePos.x = e.layerX;
-    this.creaturePos.y = e.layerY;
+    // this.creaturePos.x = e.layerX;
+    // this.creaturePos.y = e.layerY;
 
     // play sound
-    this.SoundJump.stop();
-    this.SoundJump.play();
+    // this.SoundJump.stop();
+    // this.SoundJump.play();
 
 }
 Game.prototype.onmousemove = function (e) {
@@ -76,16 +310,21 @@ Game.prototype.onkeydown = function (e) {
     // e.whitch contains charcode of pressed key
 
     // left
-    if (e.which == 37) this.creaturePos.x -= 10;
-    // right
-    if (e.which == 39) this.creaturePos.x += 10;
-    // up
-    if (e.which == 38) this.creaturePos.y -= 10;
-    // down
-    if (e.which == 40) this.creaturePos.y += 10;
-
-    this.SoundJump.stop();
-    this.SoundJump.play();
+    switch (e.which) {
+        case 37: //left
+            bear.moveLeft()
+            ok = true
+            break;
+        case 39: // right
+            bear.moveRight()
+            break;
+        case 38: //up
+        case 40: //down
+        case 32: // space
+            pause = !pause
+    }
+    // this.SoundJump.stop();
+    // this.SoundJump.play();
 }
 
 Game.prototype.onkeypress = function (e) {
